c3a26166da174245e6e3edc702365cf4b99ee526
carmenFRP <33463902+carmenFRP@users.noreply.github.com>
2021-05-06 09:39:46 +0200

    Create leerFicheroTXT.js

---
+++ leerFicheroTXT.js
@@ -0,0 +1,8 @@
+
+
+
+document.querySelector("div:nth-child(1)").addEventListener("mouseOver",()=>{
+
+  alert("HE puesto bien el evento")
+    /*let xmlDoc = loadLDocA("leerFicheroXML.xml","xml");*/
+})
