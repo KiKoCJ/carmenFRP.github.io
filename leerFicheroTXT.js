f23cc63a07854407446191166d34436281e09934
carmenFRP <33463902+carmenFRP@users.noreply.github.com>
2021-05-06 09:45:17 +0200

    Update leerFicheroTXT.js

--- leerFicheroTXT.js
+++ leerFicheroTXT.js
@@ -1,7 +1,7 @@
 
 
 
-document.querySelector("div:nth-child(1)").addEventListener("mouseOver",()=>{
+document.querySelector("div:nth-child(1)").addEventListener("mouseover",()=>{
 
   alert("HE puesto bien el evento")
     /*let xmlDoc = loadLDocA("leerFicheroXML.xml","xml");*/
